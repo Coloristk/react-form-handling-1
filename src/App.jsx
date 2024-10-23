547d7e3ebb8245f0d60d019b3212569b83de3988
thanapakorn <thanapakorn001@gmail.com>
2024-10-23 14:17:42 +0700

    feat:add ex. answer react-handling-1

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,30 @@
 import "./App.css";
-
+import { useState } from "react";
 function App() {
+  const [inputmessage, setInputMessage] = useState(" ");
+  const [showMessage, setShowMessage] = useState("Greeting Message");
+  const handleINputMesage = (event) => {
+    setInputMessage(event.target.value);
+  };
+  const handleShowMsg = () => {
+    setShowMessage(inputmessage);
+    setInputMessage(" ");
+  };
   return (
     <div className="App">
-      <div className="greeting-container">Greeting Message</div>
+      <div className="greeting-container">{showMessage}</div>
       <div className="input-container">
         <label htmlFor="greeting-message">New Greeting Message</label>
-        <input id="greeting-message" type="text" />
+        <input
+          id="greeting-message"
+          type="text"
+          value={inputmessage}
+          onChange={handleINputMesage}
+        />
       </div>
 
       <div className="buttons">
-        <button>Update text</button>
+        <button onClick={handleShowMsg}>Update text</button>
       </div>
     </div>
   );
